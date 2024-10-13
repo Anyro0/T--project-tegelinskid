4d764dc5fd1c0a2f8ba086bb83d81949b95dc4e2
Robyn Tomson <robyntomson@gmail.com>
2024-10-13 17:29:33 +0300

    KAN-77 KAN-78

--- SaleSystemGUI/src/main/java/ee/ut/math/tvt/salessystem/ui/controllers/StockController.java
+++ SaleSystemGUI/src/main/java/ee/ut/math/tvt/salessystem/ui/controllers/StockController.java
@@ -6,6 +6,7 @@ import ee.ut.math.tvt.salessystem.dataobjects.StockItem;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
@@ -46,24 +47,42 @@ public class StockController implements Initializable {
     }
     @FXML
     public void addProductClicked() {
-        String barCode = warehouseBarCode.getText();
-        String name = warehouseName.getText();
-        String priceText = warehousePrice.getText();
-        String quantityText = warehouseAmount.getText();
+        try {
+            String barCode = warehouseBarCode.getText();
+            String name = warehouseName.getText();
+            String priceText = warehousePrice.getText();
+            String quantityText = warehouseAmount.getText();
 
-        Long id = Long.parseLong(barCode);
-        double price = Double.parseDouble(priceText);
-        int quantity = Integer.parseInt(quantityText);
+            Long id = Long.parseLong(barCode);
+            double price = Double.parseDouble(priceText);
+            int quantity = Integer.parseInt(quantityText);
+            if (price < 0) {
+                showError("Invalid price", "Price cant be a negative number.");
+                return;
+            }
+            else if (quantity <= 0) {
+                showError("Invalid amount", "Amount must be a positive number.");
+                return;
+            } else {
+                StockItem newItem = new StockItem(id, name, "", price, quantity);
 
-        StockItem newItem = new StockItem(id, name, "", price, quantity);
-
-        dao.saveStockItem(newItem);
-        refreshStockItems();
-        warehouseBarCode.clear();
-        warehouseName.clear();
-        warehousePrice.clear();
-        warehouseAmount.clear();
-
+                dao.saveStockItem(newItem);
+                refreshStockItems();
+            }
+            warehouseBarCode.clear();
+            warehouseName.clear();
+            warehousePrice.clear();
+            warehouseAmount.clear();
+        } catch (NumberFormatException e) {
+        showError("Input error", "Price and amount must be valid numbers.");
+        }
+    }
+    private void showError(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
     }
     @FXML
     public void refreshButtonClicked() {
