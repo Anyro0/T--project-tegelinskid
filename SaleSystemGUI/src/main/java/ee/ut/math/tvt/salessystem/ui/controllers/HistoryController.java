a77a73fb9add404f4ab20a0f14b6fb3acb8fac75
Robyn Tomson <robyntomson@gmail.com>
2024-10-28 21:39:11 +0200

    added loging to fixed loging in SalesSystemUI and HistoryController KAN-112

--- SaleSystemGUI/src/main/java/ee/ut/math/tvt/salessystem/ui/controllers/HistoryController.java
+++ SaleSystemGUI/src/main/java/ee/ut/math/tvt/salessystem/ui/controllers/HistoryController.java
@@ -1,5 +1,6 @@
 package ee.ut.math.tvt.salessystem.ui.controllers;
 
+import ee.ut.math.tvt.salessystem.ui.SalesSystemUI;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.DatePicker;
@@ -7,14 +8,28 @@ import javafx.scene.control.DatePicker;
 import java.net.URL;
 import java.time.LocalDate;
 import java.util.ResourceBundle;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * Encapsulates everything that has to do with the purchase tab (the tab
  * labelled "History" in the menu).
  */
 public class HistoryController implements Initializable {
+    private static final Logger log = LogManager.getLogger(SalesSystemUI.class);
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        // TODO: implement
+        log.info("Initializing HistoryController");
+
+        try {
+            log.debug("HistoryController initialization details - location: {}, resources: {}", location, resources);
+
+            // Future implementation of initialization logic can go here
+
+            log.info("HistoryController initialized successfully");
+        } catch (Exception e) {
+            log.error("Error during HistoryController initialization", e);
+        }
     }
 }
