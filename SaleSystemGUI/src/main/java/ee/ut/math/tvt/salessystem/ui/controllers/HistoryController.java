b9c704fb28e34380e029574bdf91e063b9b2f7f0
KelderAnd <146181014+KelderAnd@users.noreply.github.com>
2024-10-28 21:14:00 +0200

    Added new file HistoryTab.fxml file that add history tab to GUI. KAN-63, KAN-62, KAN-61, KAN-60, KAN-57, KAN-75, KAN-74, KAN-73

--- SaleSystemGUI/src/main/java/ee/ut/math/tvt/salessystem/ui/controllers/HistoryController.java
+++ SaleSystemGUI/src/main/java/ee/ut/math/tvt/salessystem/ui/controllers/HistoryController.java
@@ -1,8 +1,11 @@
 package ee.ut.math.tvt.salessystem.ui.controllers;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.DatePicker;
 
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
 
 /**
@@ -10,7 +13,20 @@ import java.util.ResourceBundle;
  * labelled "History" in the menu).
  */
 public class HistoryController implements Initializable {
+    @FXML
+    private DatePicker startDatePicker;
 
+    @FXML
+    private DatePicker endDatePicker;
+
+    @FXML
+    public void initialize() {
+        // Set the start date to today's date
+        startDatePicker.setValue(LocalDate.now());
+
+        // Set the end date to tomorrow's date
+        endDatePicker.setValue(LocalDate.now().plusDays(1));
+    }
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         // TODO: implement
