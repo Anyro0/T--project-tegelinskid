4e52e9ad1197712b55fe92e3b7e9439985724f8b
Robyn Tomson <robyntomson@gmail.com>
2024-10-28 22:05:30 +0200

    added loging to ShoppingCart KAN-112

--- src/main/java/ee/ut/math/tvt/salessystem/logic/ShoppingCart.java
+++ src/main/java/ee/ut/math/tvt/salessystem/logic/ShoppingCart.java
@@ -3,65 +3,78 @@ package ee.ut.math.tvt.salessystem.logic;
 import ee.ut.math.tvt.salessystem.SalesSystemException;
 import ee.ut.math.tvt.salessystem.dao.SalesSystemDAO;
 import ee.ut.math.tvt.salessystem.dataobjects.SoldItem;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ShoppingCart {
 
+    private static final Logger log = LogManager.getLogger(ShoppingCart.class);
     private final SalesSystemDAO dao;
     private final List<SoldItem> items = new ArrayList<>();
 
     public ShoppingCart(SalesSystemDAO dao) {
         this.dao = dao;
+        log.info("ShoppingCart initialized.");
     }
 
     /**
      * Add new SoldItem to table or update the table if one of the same item is already in the table
      */
     public void addItem(SoldItem item) {
-
+        log.info("Attempting to add item to cart: {}, quantity: {}", item.getName(), item.getQuantity());
         int warehouseQuantity = dao.findStockItem(item.getStockItem().getId()).getQuantity();
-        if (warehouseQuantity - item.getQuantity() < 0) throw new SalesSystemException("Product amount exceeds what is available in stock");
+        log.debug("Warehouse quantity for {} is {}", item.getName(), warehouseQuantity);
+
+        if (warehouseQuantity - item.getQuantity() < 0) {
+            log.error("Product quantity exceeds available stock for {}. Requested: {}, Available: {}", item.getName(), item.getQuantity(), warehouseQuantity);
+            throw new SalesSystemException("Product amount exceeds what is available in stock");
+        }
 
         for (SoldItem itemAlreadyInBasket : items) {
             if (item.equals(itemAlreadyInBasket)) {
-                if (warehouseQuantity - (item.getQuantity() + itemAlreadyInBasket.getQuantity()) < 0)
+                int combinedQuantity = item.getQuantity() + itemAlreadyInBasket.getQuantity();
+                if (warehouseQuantity - combinedQuantity < 0) {
+                    log.error("Combined quantity of {} exceeds stock. Requested: {}, In cart: {}, Available: {}", item.getName(), item.getQuantity(), itemAlreadyInBasket.getQuantity(), warehouseQuantity);
                     throw new SalesSystemException("Product amount + same product already in cart exceeds what is available in stock");
-                else {
-                itemAlreadyInBasket.setQuantity(itemAlreadyInBasket.getQuantity() + item.getQuantity());
-                return;
+                } else {
+                    itemAlreadyInBasket.setQuantity(combinedQuantity);
+                    log.info("Updated quantity for item in cart: {}, new quantity: {}", item.getName(), combinedQuantity);
+                    return;
                 }
             }
         }
 
         items.add(item);
-        //log.debug("Added " + item.getName() + " quantity of " + item.getQuantity());
+        log.info("Added new item to cart: {}, quantity: {}", item.getName(), item.getQuantity());
     }
 
     public List<SoldItem> getAll() {
+        log.debug("Retrieving all items from cart. Current size: {}", items.size());
         return items;
     }
 
     public void cancelCurrentPurchase() {
+        log.info("Cancelling current purchase. Clearing {} items from cart.", items.size());
         items.clear();
     }
 
     public void submitCurrentPurchase() {
-        // TODO decrease quantities of the warehouse stock
-
-        // note the use of transactions. InMemorySalesSystemDAO ignores transactions
-        // but when you start using hibernate in lab5, then it will become relevant.
-        // what is a transaction? https://stackoverflow.com/q/974596
+        log.info("Submitting current purchase with {} items.", items.size());
         dao.beginTransaction();
         try {
             for (SoldItem item : items) {
                 dao.saveSoldItem(item);
+                log.debug("Saved item to purchase history: {}, quantity: {}", item.getName(), item.getQuantity());
             }
             dao.commitTransaction();
+            log.info("Purchase submitted successfully. Clearing cart.");
             items.clear();
         } catch (Exception e) {
             dao.rollbackTransaction();
+            log.error("Error during purchase submission. Transaction rolled back.", e);
             throw e;
         }
     }
