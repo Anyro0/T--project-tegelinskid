79a04bb1904471188d95aa51bb69cadd0b45d5f5
Snuukk <karl.kaplinski@gmail.com>
2024-10-28 22:46:53 +0200

    added test for transactions in warehouse KAN-114

--- src/test/java/StockBasketTest.java
+++ src/test/java/StockBasketTest.java
@@ -4,12 +4,18 @@ import ee.ut.math.tvt.salessystem.dao.SalesSystemDAO;
 import ee.ut.math.tvt.salessystem.dataobjects.StockItem;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 import ee.ut.math.tvt.salessystem.logic.StockBasket;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.*;
+import org.mockito.InOrder;
+import org.mockito.Mock;
+
+import java.util.List;
 
 public class StockBasketTest {
+    @Mock
     private SalesSystemDAO dao;
 
     private StockBasket stockBasket;
@@ -29,7 +35,8 @@ public class StockBasketTest {
     }
     @Test
     public void testAddingNewItem () {
-        assertTrue(stockBasket.addProductToStock(81L, "uusim asi", 2, 1));
+        List<StockItem> ids = dao.findStockItems();
+        assertTrue(stockBasket.addProductToStock(ids.getLast().getId()+1, "uusim asi", 2, 1));
     }
     @Test
     public void testAddingExistingItem () {
@@ -37,6 +44,17 @@ public class StockBasketTest {
         int exp = test.getQuantity() + 5;
         stockBasket.updateProductQuantity(1L,5,test.getName(),test.getPrice());
         assertEquals(exp,dao.findStockItem(1L).getQuantity());
+    }
+    @Test
+    public void testAddingItemBeginsAndCommitsTransaction () {
+        SalesSystemDAO mockdao = mock(SalesSystemDAO.class);
+        stockBasket = new StockBasket(mockdao);
+        stockBasket.addProductToStock(1L,"",1,1);
+        verify(mockdao, times(1)).beginTransaction();
+        verify(mockdao, times(1)).commitTransaction();
+        InOrder inOrder = inOrder(mockdao);
+        inOrder.verify(mockdao).beginTransaction();
+        inOrder.verify(mockdao).commitTransaction();
     }
 
 }
